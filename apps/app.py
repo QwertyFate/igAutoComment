6137ad8b5f0f92245ebb994b1236f0ca5e218198
nafaryusnarga <nafaryusnarga@gmail.com>
2025-01-14 23:27:01 +0800

    trying 2 accounts

--- apps/app.py
+++ apps/app.py
@@ -7,10 +7,11 @@ import logging
 
 logger = logging.getLogger()
 load_dotenv()
-igEmail = os.getenv("EMAIL")
-igPW = os.getenv("PASSWORD")
+igEmail = os.getenv("EMAIL1")
+igPW = os.getenv("PASSWORD1")
 cl = Client()   
 app = Flask(__name__)
+listofsession = {"qwertymake001@gmail.com" : "session.json", "instaqwertygram001@gmail.com" : "session1.json"}
 
 @app.route("/")
 def home():
@@ -20,13 +21,14 @@ def home():
 @app.route("/registerig", methods=["POST"])
 def loginIG():
     cl.login(igEmail, igPW)
-    cl.dump_settings("apps/session.json")
+    cl.dump_settings(f"session.json")
+    return "successfully created session data"
 
 @app.route("/comment", methods=["POST"])
 def comment():
     data = request.get_json()
     cl = Client()
-    cl.load_settings("apps/session.json")
+    cl.load_settings(listofsession[data["igEmail"]])
     cl.login (data["igEmail"], data["igPW"]) # this doesn't actually login using username/password but uses the session
     cl.get_timeline_feed()
     media_id = cl.media_id(cl.media_pk_from_url(data["instagramURL"]))
@@ -36,8 +38,8 @@ def comment():
 @app.route("/", methods=["POST"])
 def hello_world():
     data = request.get_json()
-    print(igPW)
-    return data["igPW"]
+    print(data["igEmail"])
+    return listofsession[data["igEmail"]]
 
 
 
